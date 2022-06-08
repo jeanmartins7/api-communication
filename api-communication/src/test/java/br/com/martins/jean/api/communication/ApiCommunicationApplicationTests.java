5259b16974b39a6f0a0d89788c6305754a4d48ac
Jean Martins <jean.martins@sensedia.com>
2022-06-07 23:19:35 -0300

    Start project

---
+++ api-communication/src/test/java/br/com/martins/jean/api/communication/ApiCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.martins.jean.api.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCommunicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
